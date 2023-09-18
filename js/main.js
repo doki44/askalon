bfefd28fcf9a6bcb703378548e27331cfaee7a23
doki44 <62612953+doki44@users.noreply.github.com>
2023-09-18 14:49:00 +0300

    Мобилка

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@
 $('.top-btn').click(function () {
     $('body,html').animate({ scrollTop: 0 }, 500);
 });
+
+$('.header__burger').on("click", function(){
+    $('.header__burger, .header__menu').toggleClass('active'); 
+    $('body').toggleClass('lock');
+});
